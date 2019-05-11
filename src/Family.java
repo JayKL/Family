895188d657a60d8d32f7195c8cc99ca1044ba43b
JayKL <jaykonrad-lee@hotmail.com>
2019-05-11 20:22:23 +0100

    first commit

---
+++ src/Family.java
@@ -0,0 +1,38 @@
+
+public class Family {
+		public boolean male(String name) {
+			
+		return true;
+		}
+		
+		public boolean female(String name) {
+			
+			
+		return true;
+		}
+		
+		public boolean isMale(String name) {
+			
+		return true;	
+		}
+		
+		public boolean isFemale(String name) {
+			
+		return true;	
+		}
+		
+		public boolean setParent(String childname, String parentname) {
+			
+		return false;
+		}
+		
+		public String[] getParents(String name){
+		String parentsarry[]= {"test"};
+		return parentsarry;
+		}
+		
+		public String[] getChildren(String name){
+			String childrenarray[]= {"test"};
+			return childrenarray;
+		}
+}
---
+++ src/FamilyTester.java
@@ -0,0 +1,49 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FamilyTester {
+	String adult="adult";
+	String child="child";
+	String testarray[]= {"test"};
+	Family fam=new Family();
+	@Test
+	void testMale() {
+	
+	assertTrue(fam.male(adult));
+	}
+	@Test
+	void testFemale() {
+	
+	assertTrue(fam.female(adult));
+	}
+	@Test
+	void testisMale() {
+	
+	assertTrue(fam.isMale(adult));
+	}
+	@Test
+	void testisFemale() {
+	
+	assertTrue(fam.isFemale(adult));
+	}
+	@Test
+	void testsetParents() {
+	
+	assertFalse(fam.setParent(child,adult));
+	}
+	@Test
+	void testgetParents() {
+	
+	assertArrayEquals(testarray,fam.getParents("hi"));
+	}
+	@Test
+	void testgetChildren() {
+	
+		assertArrayEquals(testarray,fam.getChildren("hi"));
+	}
+
+	
+	
+
+}
